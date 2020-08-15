debea6d45efc1ccff1e71341ee9c937bdf093929
Noe Garcia <jnoe.garciar@gmail.com>
2020-08-14 22:17:32 -0500

    Graficas funcionales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LineaComponent} from './components/linea/linea.component';
+import {BarrasComponent} from './components/barras/barras.component';
+import {DonaComponent} from './components/dona/dona.component';
+import {RadarComponent} from './components/radar/radar.component';
+import {PastelComponent} from './components/pastel/pastel.component';
 
 const routes: Routes = [
   { path:'linea', component: LineaComponent },
+  { path:'barra', component: BarrasComponent },
+  { path:'dona', component: DonaComponent },
+  { path:'radar', component: RadarComponent },
+  { path:'pastel', component: PastelComponent },
   { path:'**', pathMatch:'full', redirectTo: 'linea'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppComponent } from './app.component';
 //Graficos
 import { ChartsModule } from 'ng2-charts';
 import { LineaComponent } from './components/linea/linea.component';
+import { BarrasComponent } from './components/barras/barras.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+import { PastelComponent } from './components/pastel/pastel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LineaComponent
+    LineaComponent,
+    BarrasComponent,
+    DonaComponent,
+    RadarComponent,
+    PastelComponent
   ],
   imports: [
     BrowserModule,
