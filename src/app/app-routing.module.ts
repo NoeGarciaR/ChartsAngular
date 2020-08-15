740f9f0eaa96c6d165e868bff371f1d5c8933e1e
Noe Garcia <jnoe.garciar@gmail.com>
2020-08-14 21:51:05 -0500

    Gráfica de linea

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LineaComponent} from './components/linea/linea.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'linea', component: LineaComponent },
+  { path:'**', pathMatch:'full', redirectTo: 'linea'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
